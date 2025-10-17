cecfd0b51b5602f067016d756c7cc10f0f477c34
ChrisSha0104 <shashuo0104@gmail.com>
2025-10-17 13:58:11 -0400

    added runnable factory xarm task

---
+++ source/isaaclab_tasks/isaaclab_tasks/direct/factory_xarm/__init__.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import gymnasium as gym
+
+from . import agents
+from .factory_env import FactoryEnv
+from .factory_env_cfg import FactoryTaskGearMeshCfg, FactoryTaskNutThreadCfg, FactoryTaskPegInsertCfg
+
+##
+# Register Gym environments.
+##
+
+gym.register(
+    id="Isaac-Factory-Xarm-PegInsert-Direct-v0",
+    entry_point="isaaclab_tasks.direct.factory_xarm:FactoryEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": FactoryTaskPegInsertCfg,
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+    },
+)
+
+gym.register(
+    id="Isaac-Factory-Xarm-GearMesh-Direct-v0",
+    entry_point="isaaclab_tasks.direct.factory_xarm:FactoryEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": FactoryTaskGearMeshCfg,
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+    },
+)
+
+gym.register(
+    id="Isaac-Factory-Xarm-NutThread-Direct-v0",
+    entry_point="isaaclab_tasks.direct.factory_xarm:FactoryEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": FactoryTaskNutThreadCfg,
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+    },
+)
