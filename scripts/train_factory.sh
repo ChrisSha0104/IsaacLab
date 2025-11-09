0916915af3759e589553a4d1e92eaa183aad4ed3
shuosha <shashuo0104@gmail.com>
2025-11-09 14:39:40 -0500

    debug

--- scripts/train_factory.sh
+++ scripts/train_factory.sh
@@ -5,5 +5,5 @@
 
 CUDA_VISIBLE_DEVICES=0 python scripts/reinforcement_learning/rl_games/train.py \
     --task Isaac-Factory-Xarm-GearMesh-Residual  --num_envs 128 \
-    --track --wandb-project-name FactoryXarm --wandb-name xarm-gearmesh-residual-v0 --wandb-entity ss7050-columbia \
+    --track --wandb-project-name FactoryXarm --wandb-name xarm-gearmesh-residual-v1 --wandb-entity ss7050-columbia \
     --headless
